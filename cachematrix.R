c974a383a1c15141e302be26787cb25db4cc81d1
Jennifer CM Wright <jennifer.cm.wright@gmail.com>
2016-05-15 12:58:16 -0400

    my completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions create a default matrix and 
 
-## Write a short comment describing this function
+## The makeCacheMatrix function sets default values for the matrix and its inverse (default value of the inverse is NULL).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function checks to see if an inverse is already stored for the matrix. 
+## If the inverse is not stored (it is NULL), it computes the inverse of the matrix and stores the result.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached inverse")
+                return(i)
+        }
+        inverse <- x$get()
+        i <- solve(inverse, ...)
+        x$setinverse(i)
+        i
 }
